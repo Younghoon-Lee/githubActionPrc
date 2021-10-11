0594e8765862117937925fe81433b162942e736a
Younghoon-Lee <8109h@naver.com>
2021-10-11 23:33:07 +0900

    slack bot test

---
+++ bot.py
@@ -0,0 +1,20 @@
+import requests
+import json
+def post_message(channel, text): 
+    SLACK_BOT_TOKEN = ""
+    headers = {
+        'Content-Type': 'application/json', 
+        'Authorization': 'Bearer ' + SLACK_BOT_TOKEN
+        }
+    payload = {
+        'channel': channel,
+        'text': text
+        }
+    r = requests.post('https://slack.com/api/chat.postMessage', 
+        headers=headers, 
+        data=json.dumps(payload)
+        )
+if __name__ == '__main__':
+    post_message("#thundervalley", "test")
+
+
